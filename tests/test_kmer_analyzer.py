4c47d73395b4841f93b47d6deb62223c08d38ea4
calllummm <158099114+calllummm@users.noreply.github.com>
2026-05-06 12:02:09 -0400

    added parametrize decorator, tested validate sequence function

--- tests/test_kmer_analyzer.py
+++ tests/test_kmer_analyzer.py
@@ -1,3 +1,21 @@
+# relevant imports
+import pytest 
+from kmer_analyzer import validate_sequence
+
+# dynamic test functionality through parametrize
+@pytest.mark.parametrize(
+    "sequence,k,expected",
+    [
+        ("ACGT", 2, True),
+        ("A", 2, False),
+        ("AC1T", 2, False),
+    ],
+)
+
+# test function to validate sequence checking logic
+def test_validate_sequence(sequence, k, expected):
+    assert validate_sequence(sequence, k) is expected
+
 # basic pytest confirmation
 def test_pytest_working():
     assert True
